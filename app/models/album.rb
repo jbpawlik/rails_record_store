f5416d691617cf10ade5e23f7eecb9e19fc36688
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-08-03 18:04:45 -0700

    add to repo

---
+++ app/models/album.rb
@@ -0,0 +1,13 @@
+class Album < ApplicationRecord
+  has_and_belongs_to_many(:artists)
+  has_many :songs, dependent: :destroy
+  validates :name, presence: true
+  validates_length_of :name, maximum: 100
+
+  before_save(:titleize_album)
+
+  private
+    def titleize_album
+      self.name = self.name.titleize
+    end
+end
